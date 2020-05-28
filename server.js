eda302973b777e54703f0188674223e8701cdef0
luizzitol <62354883+luizzitol@users.noreply.github.com>
2020-05-28 12:26:28 +0100

    added comment on server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Connecting to Database
 require("./server/database/database")();
+
 app.get("/", (req, res) => {
   res.json("Hi from the server");
 });
